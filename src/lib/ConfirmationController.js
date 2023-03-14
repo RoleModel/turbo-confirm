97d50f90b2606fbe70d3c2063b9e960cccd485e7
Andy Cohen <outlawandy@gmail.com>
2023-03-14 10:28:36 -0500

    store and restore dialog content

--- src/lib/ConfirmationController.js
+++ src/lib/ConfirmationController.js
@@ -1,5 +1,6 @@
 export default class ConfirmationController {
   #originalSubmitter
+  #initialContent
   #config = {
     dialogSelector: '#confirm',
     activeClass: 'modal--active',
@@ -62,6 +63,9 @@ export default class ConfirmationController {
   }
 
   #showConfirm(element) {
+    // if this is the first time, store the HTML of the dialog.
+    // We'll use this to restore the dialog to its original state on teardown.
+    if (!this.#initialContent) this.#initialContent = this.dialogTarget.innerHTML
     this.#fillSlots(element)
     this.dialogTarget.classList.add(this.#config.activeClass)
     this.#setupListeners()
@@ -71,7 +75,7 @@ export default class ConfirmationController {
     this.dialogTarget.classList.remove(this.#config.activeClass)
     this.#originalSubmitter = undefined
     this.#teardownListeners()
-    this.#clearSlots()
+    this.#restoreDialog()
   }
 
   #fillSlots(sourceElement) {
@@ -85,13 +89,10 @@ export default class ConfirmationController {
     }
   }
 
-  #clearSlots() {
+  #restoreDialog() {
     // allow for hide animation to complete before removing content
     setTimeout(()=> {
-      for(const slotName of Object.keys(this.#config.contentSlots)) {
-        const slotTarget = this.#slotTarget(slotName)
-        if (slotTarget) slotTarget.innerHTML = ''
-      }
+      this.dialogTarget.innerHTML = this.#initialContent
     }, this.#config.animationDuration)
   }
 
